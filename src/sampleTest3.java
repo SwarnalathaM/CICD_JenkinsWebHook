d2ec13590e81290e915d3131f06af9fe27f6a235
Swarna <swarnam0302@gmail.com>
2026-03-20 10:19:34 +0530

    develop3 branch update

---
+++ src/sampleTest3.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class sampleTest3 {
+	@Test
+	public void getURLEdge1() {
+//		public static void main(String[] args) {
+			WebDriver driver=new EdgeDriver();
+			driver.get("https://rahulshettyacademy.com/");
+			System.out.println(driver.getTitle()+ " from Edge");
+System.out.println("Edge in test 3");
+			driver.close();
+			}
+	@Test
+	public void getURLEdge2() {
+//		public static void main(String[] args) {
+			WebDriver driver=new EdgeDriver();
+			driver.get("https://google.com/");
+			System.out.println(driver.getTitle()+ " from Edge");
+System.out.println("Edge in test 3");
+			driver.close();
+			}
+}
